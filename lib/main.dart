96b66f38a0b02f15092a7016c169a111a53f960c
jiho <jhkkim0114@gmail.com>
2022-04-18 00:02:51 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/util/app_scroll_behavior.dart';
 
 void main() {
   runApp(GetMaterialApp(
-    initialRoute: Routes.ground,
+    initialRoute: Routes.main,
     debugShowCheckedModeBanner: false,
     theme: appThemeData,
     defaultTransition: Transition.fade,
